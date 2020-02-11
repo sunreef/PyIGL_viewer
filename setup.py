8d49a7145ee70a782601291c728c0d61dee5d0f7
Victor Cornillere <v.cornillere@gmail.com>
2020-02-11 19:34:24 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(name='IGL_viewer',
+      version='0.1',
+      description='Python IGL viewer',
+      long_description='Because sometimes Matplotlib is not enough.',
+      url='https://TBD',
+      classifiers=[
+        'Programming Language :: Python :: 3.7',
+      ],
+      keywords='igl viewer opengl mesh display',
+      author='Victor Cornillère',
+      author_email='covictor@inf.ethz.ch',
+      packages=find_packages(),
+      install_requires=[
+      ],
+      include_package_data=True,
+      zip_safe=False)
---
+++ viewer/viewer.py
@@ -0,0 +1,8 @@
+
+
+class Viewer:
+    def __init__(self):
+        pass
+
+
+
