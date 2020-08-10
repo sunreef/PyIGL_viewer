4b04ba5f2f0b7b1d9b0ab903243a51e284a3984f
Victor Cornillere <covictor@inf.ethz.ch>
2020-08-10 15:10:53 +0200

    Add some comments to normal example

--- examples/normal_display.py
+++ examples/normal_display.py
@@ -6,7 +6,7 @@ from PyIGL_viewer import Viewer
 
 script_folder = os.path.dirname(__file__)
 path_to_obj_file = os.path.join(script_folder, 'assets', 'cube.obj')
-# Path to your OBJ file stored in path_to_obj_file 
+# Path to your OBJ file stored in path_to_obj_file
 vertices, faces = igl.read_triangle_mesh(path_to_obj_file)
 
 bbox_min = np.min(vertices, axis=0)
@@ -31,6 +31,8 @@ viewer_widget.link_light_to_camera()
 # Add a screenshot button
 def screenshot_function():
     viewer.save_screenshot(os.path.join('screenshot.png'))
+
+
 viewer.add_ui_button("Take Screenshot", screenshot_function)
 
 
@@ -39,13 +41,18 @@ viewer.add_ui_button("Take Screenshot", screenshot_function)
 # - Adding the mesh vertices and faces
 # - Adding a mesh prefab that contains shader attributes and uniform values
 # - Adding an instance of our prefab whose position is defined by a model matrix
+
+# Here, we use the lambert shader.
+# This shader requires two things:
+# - A uniform value called 'albedo' for the color of the mesh.
+# - An attribute called 'normal' for the mesh normals.
 uniforms = {}
 vertex_attributes = {}
 face_attributes = {}
 
 uniforms['albedo'] = np.array([0.8, 0.8, 0.8])
 
-# If we want flat shading with normals deinfed per face.
+# If we want flat shading with normals defined per face.
 face_normals = igl.per_face_normals(vertices, faces, np.array([1.0, 1.0, 1.0])).astype(np.float32)
 face_attributes['normal'] = face_normals
 
@@ -54,10 +61,11 @@ face_attributes['normal'] = face_normals
 # vertex_attributes['normal'] = vertex_normals
 
 mesh_index = viewer_widget.add_mesh(vertices, faces)
-mesh_prefab_index = viewer_widget.add_mesh_prefab(mesh_index, 'lambert', vertex_attributes = vertex_attributes, face_attributes=face_attributes, uniforms=uniforms)
+mesh_prefab_index = viewer_widget.add_mesh_prefab(
+    mesh_index, 'lambert', vertex_attributes=vertex_attributes, face_attributes=face_attributes, uniforms=uniforms)
 instance_index = viewer_widget.add_mesh_instance(mesh_prefab_index, np.eye(4, dtype='f'))
 
-# Add the wireframe for our mesh 
+# Add the wireframe for our mesh
 # The wireframe shader expect a uniform attribute called lineColor that specifies the color of the wireframe lines.
 viewer_widget.add_wireframe(instance_index, line_color=np.array([0.1, 0.1, 0.1]))
 
